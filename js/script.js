0fe200ae0b892d340bb99ba45001883569c6ef23
Joanna Smukala <joasmu@wp.pl>
2018-03-29 09:51:09 +0200

    zadanie5

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var a = prompt ("twoja liczba");
+var h = prompt ("wysokość");
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
